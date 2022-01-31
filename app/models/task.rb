67d0b8c0942e01cad9ec5f08e7c45c87be7d1449
fumtas1k <fumta01a7@gmail.com>
2022-01-31 15:30:49 +0900

    improve:taskのnameの文字数制限を30文字に設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 30 }
 end
---
+++ db/migrate/20220131062731_change_column_limit_to_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeColumnLimitToTasks < ActiveRecord::Migration[6.0]
+  def change
+    change_column :tasks, :name, :string, limit: 30
+  end
+end
