27bf5c85b5961eb1a598df2c7052fbadacefd6f5
fumtas1k <fumta01a7@gmail.com>
2022-01-31 15:24:16 +0900

    add:taskの新規登録、編集画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show]
+  before_action :set_task, only: %i[show edit update]
   def index
     @tasks = Task.all
   end
@@ -7,7 +7,37 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:succss] = "#{@task.name} を新規登録しました!"
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = "#{@task.name} を編集しました!"
+      redirect_to @task
+    else
+      render :edit
+    end
+  end
+
   private
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
   def set_task
     @task = Task.find(params[:id])
   end
