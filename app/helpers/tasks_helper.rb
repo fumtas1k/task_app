a35d089aa91ff3dd167792605b48f0936f14e311
fumtas1k <fumta01a7@gmail.com>
2022-02-04 10:47:59 +0900

    improve:task indexビューでの条件分岐をhelperメソッド化

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,4 +3,13 @@ module TasksHelper
     direction = (sort_column == column && sort_direction == "desc") ? "asc" : "desc"
     link_to column_name, { column: column, direction: direction }.merge(Hash(hash_params))
   end
+
+  def hash_nil_guard(hash, *keys)
+    ans = Hash(hash)
+    keys.each do |key|
+      return nil unless [Hash, ActionController::Parameters].include?(ans.class)
+      ans = ans[key]
+    end
+    return ans
+  end
 end
